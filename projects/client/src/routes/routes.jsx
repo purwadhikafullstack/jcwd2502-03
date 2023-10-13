b7d2cdbb97eceeb326224ff2f8b61d2de12141ad
caraka wijaya <109886433+caraka20@users.noreply.github.com>
2023-10-13 15:39:36 +0700

    added footer component

--- projects/client/src/routes/routes.jsx
+++ projects/client/src/routes/routes.jsx
@@ -1,7 +1,10 @@
 import { Route } from "react-router-dom";
 import Button from "../components/Button/Button";
 import Nav from "../components/Navbar/Nav";
+import Footer from "../components/Footer/Footer";
 
-const routes = [<Route path="/" element={<Nav />} />];
+const routes = [
+<Route path="/" element={<Footer />} />
+];
 
 export default routes;
