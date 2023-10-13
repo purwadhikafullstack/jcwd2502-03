ee82821ab44191b0ed3c775293ade4ed01bbf5c9
caraka wijaya <109886433+caraka20@users.noreply.github.com>
2023-10-13 10:03:53 +0700

    setup frontend

--- projects/client/tailwind.config.js
+++ projects/client/tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryOrange: 'rgba(250, 130, 50, 1)'
+      }
+    },
   },
   plugins: [],
 }
