3af72f5eb6050e3bdb6e3126c9958834d9048ede
Jordan <74591126+jordansmrd@users.noreply.github.com>
2023-10-24 14:23:32 +0700

    Update api.js

--- projects/client/src/config/api.js
+++ projects/client/src/config/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-export const API_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8000";
+export const API_URL = process.env.REACT_APP_API_BASE_URL
 
-const axiosInstance = axios.create({ baseURL: API_URL });
+const axiosInstance = axios.create({ baseURL: process.env.REACT_APP_API_BASE_URL });
 
 axiosInstance.interceptors.request.use((config) => {
     const userToken = Cookies.get("user_token");
